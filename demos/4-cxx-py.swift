13a30c2155947ded0e8384411a3625918b872c12
Justin Wozniak <wozniak@mcs.anl.gov>
2018-05-22 16:52:34 -0500

    Return a string

--- demos/4-cxx-py.swift
+++ demos/4-cxx-py.swift
@@ -3,8 +3,11 @@ import python;
 
 f()
 {
-  python("import python_wrapper",
-         "python_wrapper.f()");
+  python("""
+import python_wrapper
+python_wrapper.f()
+""",
+         "repr(0)");
 }
 
 f();
