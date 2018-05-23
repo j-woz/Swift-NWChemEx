0d11579a73fcb7bfe6767eb56a7a48424eb2729e
Justin Wozniak <wozniak@mcs.anl.gov>
2018-05-23 12:39:14 -0500

    Be more defensive

--- demos/python-settings.sh
+++ demos/python-settings.sh
@@ -9,8 +9,12 @@ PY_VERSION=$( python -c 'import sys;print(sys.version_info[0])' )
 if [[ $PY_VERSION == 2 ]]
 then
   PY_CONFIG=python-config
-else
+elif [[ $PY_VERSION == 3 ]]
+then
   PY_CONFIG=python3-config
+else
+  echo "Python is broken!"
+  exit 1
 fi
 
 which $PY_CONFIG
