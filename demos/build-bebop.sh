59c5bb66bcbe6a3c5f1ae69e23c42a579f30d651
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-05-23 11:01:46 -0500

    Auto-detect Python version; use new Swift/T installation

--- demos/build-bebop.sh
+++ demos/build-bebop.sh
@@ -1,14 +1,25 @@
 #!/bin/bash -l
 set -eu
 
-export TCL=/home/wozniak/Public/sfw/bebop/login/tcl-8.6.5
+export TCL=/home/wozniak/Public/sfw/bebop/login/tcl-8.6.5-g
 
 PATH=/soft/anaconda3/5.0.0/envs/tensorflow/bin:$PATH
 
-which python python-config
+which python
+
+PY_VERSION=$( python -c 'import sys;print(sys.version_info[0])' )
+
+if [[ $PY_VERSION == 2 ]]
+then
+  PY_CONFIG=python-config
+else
+  PY_CONFIG=python3-config
+fi
+
+which $PY_CONFIG
 
 export PYBIND=/home/wozniak/proj/pybind11
-export PY_INCLUDE=$( python3-config --includes )
-export PY_LIB=$( python3-config --libs )
+export PY_INCLUDE=$( $PY_CONFIG --includes )
+export PY_LIB=$( $PY_CONFIG --libs )
 
 make $*
--- demos/run-bebop-local.sh
+++ demos/run-bebop-local.sh
@@ -3,7 +3,7 @@ set -eu
 
 # RUN LOCAL
 
-PATH=/home/wozniak/Public/sfw/bebop/login/swift-t-2018-05-16/stc/bin:$PATH
+PATH=/home/wozniak/Public/sfw/bebop/login/swift-t-2018-05-22/stc/bin:$PATH
 
 # Only needed to find e.g. Tcl/C++ -based packages
 export SWIFT_PATH=$PWD
@@ -15,6 +15,6 @@ PY=/soft/anaconda3/5.0.0/envs/tensorflow
 PATH=$PY/bin:$PATH
 export LD_LIBRARY_PATH=$PY/lib
 export PYTHONHOME=$PY
-export PYTHONVERBOSE=1
+# export PYTHONVERBOSE=1
 
 nice swift-t -o t.tic -u $*
